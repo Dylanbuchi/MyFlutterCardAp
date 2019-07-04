8d1fc0c8ebccb649d9c2c5c261ffcac09876d50e
Dylan <52018183+Dylanbuchi@users.noreply.github.com>
2019-07-04 20:14:04 -0300

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.cyan[700],
         body: SafeArea(
             child: Column(
           children: <Widget>[
             CircleAvatar(
               backgroundImage: AssetImage('images/dylan.jpg'),
               radius: 50,
-            )
+            ),
+            Text(
+              'Dylan Buchi',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Muli',
+                color: Colors.cyan[100],
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.4,
+                height: 1.5,
+              ),
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[Icon(Icons.add_box)],
+              ),
+            ),
           ],
         )),
       ),
