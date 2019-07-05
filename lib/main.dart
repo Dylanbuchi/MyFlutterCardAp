edcfe4c85b212aabd29902191639dd67780104ed
Dylan <52018183+Dylanbuchi@users.noreply.github.com>
2019-07-05 13:50:10 -0300

    updated 2.00

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,39 +9,83 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.cyan[700],
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              backgroundImage: AssetImage('images/dylan.jpg'),
-              radius: 50,
-            ),
-            Text(
-              'Dylan Buchi',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/dylan.jpg'),
+                radius: 50,
+              ),
+              Text(
+                'Dylan Buchi',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Muli',
+                  color: Colors.cyan[100],
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.4,
+                  height: 1.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.cyan[100],
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.cyan[700],
+                      size: 25,
+                    ),
+                    title: Text(
+                      '+(41) 77 987 27 92',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Muli',
-                color: Colors.cyan[100],
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.4,
-                height: 1.5,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.cyan[700],
+                      size: 25,
+                    ),
+                    title: Text(
+                      "dylanbuchi@gmail.com",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[Icon(Icons.add_box)],
-              ),
-            ),
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
